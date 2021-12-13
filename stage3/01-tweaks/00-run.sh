ff9a6c6255d9cc0717cef4aedceca1f2e920be82
Letterix <rasmus.letterkrantz@gmail.com>
2021-12-13 16:26:34 +0100

    WIP: Moved alsa config and node to existing stages

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -4,12 +4,6 @@ on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 EOF
 
-#  Install NodeJS
-on_chroot <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-  apt-get install -y nodejs
-EOF
-
 #  Install PM2
 on_chroot <<EOF
   npm install pm2 -g
@@ -33,14 +27,4 @@ cat <<EOT >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
 export UPDATER_USERNAME="<STREAMBOKS_CHANGE>"
 export UPDATER_PASSWORD="<STREAMBOKS_CHANGE>"
 export UPDATER_URL="<STREAMBOKS_CHANGE>"
-EOT
-
-# Set alsa config, given the Hifiberry config above
-cat <<EOT > ${ROOTFS_DIR}/etc/asound.conf
-pcm.!default {
-	type hw card 0
-}
-ctl.!default {
-	type hw card 0
-}
 EOT
