693249782ae60bee09c2813f41da288c0ce73116
Letterix <rasmus.letterkrantz@gmail.com>
2021-12-16 15:33:28 +0100

    WIP: Now installs nodejs via nvm

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -4,26 +4,40 @@ on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 EOF
 
-#  Install PM2
-on_chroot <<EOF
-  npm install pm2 -g
+# Install NVM
+on_chroot << EOF
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 EOF
 
+# Install NodeJS 16 and make it default (via nvm)
+on_chroot << EOF
+  nvm install 16
+  nvm alias default 16
+EOF
+
+#  Install and configure PM2
+on_chroot <<EOF
+  npm install pm2 -g
+  pm2 startup
+  env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u soundboks --hp ${ROOTFS_DIR}/home/${FIRST_USER_NAME}
+EOF
+
+# Add the pm2 ecosystem for sb-streamboks
+install -m 644 files/ecosystem.config.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+
 # Add sb-nodejs-bootloader
-mkdir ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/sb-nodjs-bootloader
-unzip ${BASE_DIR}/sb-base-code/sb-nodejs-bootloader.zip -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/sb-nodjs-bootloader
+mkdir ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/sb-nodejs-bootloader
+unzip files/sb-nodejs-bootloader.zip -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/sb-nodejs-bootloader
 
 # Add the bootloader app config for sb-streamboks
-cat <<EOT > ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/sb-nodjs-bootloader/apps/.sb-streamboks.json
-"path": "sb-streamboks",
-"repo": "streamboks",
-"file": "dist.zip",
-"main": "src/index.js",
-"busy": false
-EOT
+install -m 644 files/ecosystem.config.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/sb-nodejs-bootloader/apps/.sb-streamboks.json"
 
 # Add placeholder env variables for the apps
 cat <<EOT >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
+
+# Custom env variables
+export WS_ID="<STREAMBOKS_CHANGE>"
+export WS_URL="<STREAMBOKS_CHANGE>"
 export UPDATER_USERNAME="<STREAMBOKS_CHANGE>"
 export UPDATER_PASSWORD="<STREAMBOKS_CHANGE>"
 export UPDATER_URL="<STREAMBOKS_CHANGE>"
---
+++ stage3/01-tweaks/files/ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps : [{
+    name   : "bootloader",
+    script : "./sb-nodejs-bootloader/src/index.js",
+    instance_var: 'INSTANCE_ID',
+  }]
+}
